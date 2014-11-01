51912283cec5899f8182e6a0a7847aab05307a74
Shane <shanec132006@gmail.com>
2014-10-31 18:43:32 -0700

    Updated homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+
+  # Helper for title
+
+  def full_title(page_title = "")
+    base_title = "My Portfolio"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
